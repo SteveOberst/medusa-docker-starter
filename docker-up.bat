870ea94d3c76d958e8c93fb5fdd870b821aad1c3
Steve Oberst <58824185+SteveOberst@users.noreply.github.com>
2025-08-11 10:53:50 +0200

    feat(windows): docker-up.bat supports -d/--detached and --rebuild; default attached

--- docker-up.bat
+++ docker-up.bat
@@ -1,8 +1,21 @@
 @echo off
 setlocal
-if /I "%~1"=="--rebuild" goto rebuild
-if /I "%~1"=="rebuild" goto rebuild
-docker compose up -d
-goto :eof
-:rebuild
-docker compose up --build -d
+set DETACHED=
+set REBUILD=
+
+:parse
+if "%~1"=="" goto run
+if /I "%~1"=="-d" set DETACHED=1
+if /I "%~1"=="--detached" set DETACHED=1
+if /I "%~1"=="--rebuild" set REBUILD=1
+if /I "%~1"=="rebuild" set REBUILD=1
+shift
+goto parse
+
+:run
+set BUILD=
+if defined REBUILD set BUILD=--build
+set DET=
+if defined DETACHED set DET=-d
+
+docker compose up %BUILD% %DET%
